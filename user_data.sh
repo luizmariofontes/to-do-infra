65aad8beb267318b94cdada24dfa146f324e7ee8
luizmariofontes <luizmariofontes@gmail.com>
2025-07-13 23:03:36 -0300

    build: Ajusta script de usuário

--- user_data.sh
+++ user_data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -e  # Encerra o script se qualquer comando falhar
+set -e  
 
-# Instalação de dependências e Docker
 apt-get update -y
 apt-get install -y ca-certificates curl software-properties-common gnupg lsb-release
 
@@ -15,7 +14,6 @@ https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# Instalação do Docker Compose v2
 DOCKER_COMPOSE_VERSION=v2.23.1
 mkdir -p /usr/libexec/docker/cli-plugins
 curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" \
@@ -23,21 +21,17 @@ curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VE
 
 chmod +x /usr/libexec/docker/cli-plugins/docker-compose
 
-# Habilita o Docker
 systemctl start docker
 systemctl enable docker
 
-# Adiciona o usuário 'ubuntu' ao grupo docker
 usermod -aG docker ubuntu
 
 sudo iptables -A INPUT -p tcp --dport 8000 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 8083 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 8085 -j ACCEPT
 
-# Prepara o diretório da aplicação
 
 cd /opt/app
 
-# Baixa o arquivo docker-compose
 curl -o docker-compose.yml https://raw.githubusercontent.com/luizmariofontes/to-do-infra/main/docker-compose.yaml
 
